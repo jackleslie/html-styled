465c862d09d88c007997757debd929ba7631af49
Jack Leslie <jjdl@st-andrews.ac.uk>
2020-06-01 10:40:10 +0100

    chore: docs declutter

--- docs/data/css.js
+++
@@ -1,9 +0,0 @@
-export const properties = [
-  "appearance",
-  "cursor",
-  "textDecoration",
-  "transition",
-  "whiteSpace",
-];
-
-export const selectors = ["active", "focus", "hover"];
--- docs/data/index.js
+++
@@ -1,2 +0,0 @@
-export { default as elements } from "./elements";
-export { properties, selectors } from "./css";
